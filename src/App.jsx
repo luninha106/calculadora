e41ef0f30a5f8d2fc7cdc71bd490ab47e77a44dc
luninha106 <lunagomesdesantana901@gmail.com>
2024-11-13 15:50:19 -0300

    lógica

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
 import { useState } from "react"
+//hook do rract para mudanças de estado
 import "./style.scss"
 
 export default function App () {
   //VARIAVEL DE ESTADO
-  const{primeiroValor, seteSegundoValor} =useState()
+  //primeiroValor é a variáve de estado
+  //setPrimeiroValor é a função atualizadora
+  const{primeiroValor, setPrimeiroValor} =useState()
+  const{segundoValor, setSegundoValor} =useState()
+
+  const capturandoPrimeirovalor = (e) => { 
+    setPrimeiroValor(Number(e.target.value))
+    console.log(e.target.value)
+  }
+  const capturandosegundaValor = (e) => {
+    setSegundoValor(Number(e.target.vale))
+  } 
   return (
     <main>
       <h1>Calculadora</h1>
-      <input type="number" placeholder="Insira um número"/>
-      <input type="number" placeholder="Insira um número"/>
+      <input onChange={capturandoPrimeirovalor} type="number" placeholder="Insira um número"/>
+      <input onChange={capturandosegundaValor} type="number" placeholder="Insira um número"/>
       <div>
         <button>+</button>
         <button>-</button>
